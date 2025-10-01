f10d6c2c61323d93f0e49586bbb6d61943ede45a
vylsaz <cokelover7244@proton.me>
2025-10-01 02:05:21 -0400

    Threefold

--- ic.c
+++ ic.c
@@ -4,6 +4,9 @@
 #include <assert.h>
 #include <inttypes.h>
 #include <ctype.h>
+#ifndef _WIN32
+    #include <dlfcn.h>
+#endif
 
 typedef uint64_t usz;
 typedef Nob_String_Builder StrBuilder;
@@ -239,7 +242,58 @@ void SimpleQuote(char const **opt, usz optLen, StrBuilder *out)
         nob_da_append(out, '\"');
         if (i+1<optLen) nob_da_append(out, ' ');
     }
-    nob_sb_append_null(out);
+}
+
+// temp
+void ParseShell(char const *sh, usz len, Nob_Cmd *cmd)
+{
+    usz i;
+    int inQuote = 0, wasSpace = 0;
+    Nob_String_View sv = nob_sv_from_parts(sh, len);
+    StrBuilder sb = {0};
+
+    #define FINISH_ARG do {\
+        nob_sb_append_null(&sb);\
+        nob_da_append(cmd, nob_temp_strdup(sb.items));\
+        sb.count = 0;} while (0)
+
+    sv = nob_sv_trim(sv);
+    for (i = 0; i<sv.count; ++i) {
+        char c = sv.data[i];
+        if (inQuote==1) {
+            wasSpace = 0;
+            if (c!='\'') {
+                nob_da_append(&sb, c);
+            } else if (i+1<sv.count && sv.data[i+1]=='\'') {
+                nob_da_append(&sb, c); ++i;
+            } else {
+                inQuote = 0;
+            }
+        } else if (c=='\'') {
+            inQuote = 1;
+            wasSpace = 0;
+        } else if (c==' ') {
+            if (wasSpace==0) FINISH_ARG;
+            wasSpace = 1;
+        } else { 
+            wasSpace = 0;
+            nob_da_append(&sb, c);
+        }
+    }
+    FINISH_ARG;
+
+    #undef FINISG_ARG
+    nob_sb_free(sb);
+}
+
+void SpawnShell(char const *sh, usz len)
+{
+    Nob_Cmd cmd = {0};
+    size_t mark = nob_temp_save();
+    ParseShell(sh, len, &cmd);
+    nob_cmd_run(&cmd);
+    nob_temp_rewind(mark);
+    nob_da_free(cmd);
 }
 
 void ErrFunc(void *opaque, const char *msg)
@@ -272,14 +326,8 @@ void ErrFunc(void *opaque, const char *msg)
         "if (__LINE__>LASTLINE) "#FUNC"(__VA_ARGS__);"\
     "} while(0)\n"
 
-int Run(
-    char const *tccPath, char const *incPath, char const *libPath, usz line,
-    char const **opt, usz optLen,
-    char const **arg, usz argLen,
-    char const *pre, usz preLen, 
-    char const *first, usz firstLen, 
-    char const *src, usz srcLen, 
-    char const *last, usz lastLen)
+void PrepareCString(usz line, StrBuilder *pre, StrBuilder *first,
+    StrBuilder *src, StrBuilder *last, StrBuilder *sb)
 {
     static char include[] = 
         "#include <stdio.h>\n"
@@ -291,6 +339,7 @@ int Run(
     ;
     
     static char prolog[] = 
+        "#line 1 \"nowhere\"\n"
         BIN_FUNCTION(8) BIN_FUNCTION(16) BIN_FUNCTION(32) BIN_FUNCTION(64)
         "#define BIN(X) _Generic((X),"
             GEN_BIN(8) GEN_BIN(16) GEN_BIN(32) GEN_BIN(64) "default:__bin64)(X)\n"
@@ -355,49 +404,142 @@ int Run(
                     "if (a[j]<=126 && a[j]>=33) printf(\"%c\",a[j]); else printf(\".\");"
                 "puts(\"\");}}\n"
         PATCH(printf) PATCH(puts) PATCH(putchar)
-        "int main(int argc, char **argv) {\n"
+    #ifdef _WIN32
+        "__declspec(dllexport)"
+    #endif
+        "int ic_main(int argc, char **argv) {\n"
         "(void) argc; (void) argv;\n"
         ;
+
     static char epilog[] = "return 0;\n}\n";
-    char lastline[256] = {0};
-    static StrBuilder sb = {0}, sbo = {0};
+
+    usz mark = nob_temp_save();
+    char *lastline = nob_temp_sprintf("#define LASTLINE %zu\n", line);
+
+    nob_sb_append_cstr(sb, include);
+    nob_sb_append_buf(sb, pre->items, pre->count);
+    nob_sb_append_buf(sb, first->items, first->count);
+    nob_sb_append_cstr(sb, prolog);
+    nob_sb_append_cstr(sb, lastline);
+    nob_sb_append_buf(sb, src->items, src->count);
+    nob_sb_append_buf(sb, last->items, last->count);
+    nob_sb_append_cstr(sb, epilog);
+
+    nob_temp_rewind(mark);
+}
+
+typedef enum RunType {
+    RT_MEM,
+    RT_DLL,
+    RT_CC,
+} RunType;
+
+int Run(RunType rt,
+    char const *tccPath, char const *incPath, char const *libPath, usz line,
+    Nob_Cmd *opt, Nob_Cmd *arg, 
+    StrBuilder *pre, StrBuilder *first,
+    StrBuilder *src, StrBuilder *last)
+{
+    typedef int (*IcMain)(int, char **);
+    IcMain ic_main;
+    TCCState *s = NULL;
+    static StrBuilder sbSrc = {0}, sbOpt = {0};
     int r = -1;
-    TCCState *s = tcc_new();
     usz i, mark = nob_temp_save();
-    char **myArgs = nob_temp_alloc((1+argLen)*sizeof(char *));
+    int myArgsLen = 1+arg->count;
+    char **myArgs = nob_temp_alloc((myArgsLen)*sizeof(char *));
+#ifdef _WIN32
+    HMODULE h = NULL;
+    char const *outPath = nob_temp_sprintf("%s/ic.dll", GetExePath());
+#else
+    void *h = NULL;
+    char const *outPath = nob_temp_sprintf("%s/ic.so", GetExePath());
+#endif
 
+    // args to main
     myArgs[0] = nob_temp_strdup(GetExe(NULL));
-    for (i = 0; i<argLen; ++i) {
-        myArgs[1+i] = nob_temp_strdup(arg[i]);
+    for (i = 0; i<arg->count; ++i) {
+        myArgs[1+i] = nob_temp_strdup(arg->items[i]);
     }
 
-    sprintf(lastline, "#define LASTLINE %zu\n", line);
+    // prepare in memory c src code
+    sbSrc.count = 0;
+    PrepareCString(line, pre, first, src, last, &sbSrc);
+    if (rt!=RT_CC) nob_sb_append_null(&sbSrc);
 
-    sb.count = 0;
-    nob_sb_append_cstr(&sb, include);
-    nob_sb_append_buf(&sb, pre, preLen);
-    nob_sb_append_buf(&sb, first, firstLen);
-    nob_sb_append_cstr(&sb, prolog);
-    nob_sb_append_cstr(&sb, lastline);
-    nob_sb_append_buf(&sb, src, srcLen);
-    nob_sb_append_buf(&sb, last, lastLen);
-    nob_sb_append_cstr(&sb, epilog);
-    nob_sb_append_null(&sb);
+    if (rt==RT_CC) {
+        Nob_Log_Level old = nob_minimal_log_level;
+        char const *inpPath = nob_temp_sprintf("%s/_ic.c", GetExePath());
+        nob_write_entire_file(inpPath, sbSrc.items, sbSrc.count);
 
-    sbo.count = 0;
-    SimpleQuote(opt, optLen, &sbo);
+        Nob_Cmd cc = {0};
+        nob_da_append(&cc, "cc");
+        for (usz i = 0; i<opt->count; ++i) {
+            nob_da_append(&cc, opt->items[i]);
+        }
+        nob_da_append(&cc, "-shared");
+        nob_da_append(&cc, "-o");
+        nob_da_append(&cc, outPath);
+        nob_da_append(&cc, inpPath);
+        nob_minimal_log_level = NOB_WARNING;
 
-    tcc_set_error_func(s, NULL, ErrFunc);
-    tcc_set_lib_path(s, tccPath);
-    tcc_add_sysinclude_path(s, incPath);
-    tcc_add_library_path(s, libPath);
-    tcc_set_output_type(s, TCC_OUTPUT_MEMORY);
-    tcc_set_options(s, sbo.items);
-    r = tcc_compile_string(s, sb.items);
-    if (r!=-1) {
-        r = tcc_run(s, 1+argLen, myArgs);
+        if (!nob_cmd_run(&cc)) {
+            nob_delete_file(inpPath);
+            nob_minimal_log_level = old;
+            goto end;
+        }
+        nob_delete_file(inpPath);
+        nob_minimal_log_level = old;
+    } else {
+        // prepare quoted options
+        sbOpt.count = 0;
+        SimpleQuote(opt->items, opt->count, &sbOpt);
+        nob_sb_append_null(&sbOpt);
+        
+        // compile by tcc
+        s = tcc_new();
+        tcc_set_output_type(s, rt==RT_DLL? TCC_OUTPUT_DLL: TCC_OUTPUT_MEMORY);
+        tcc_set_options(s, sbOpt.items);
+        tcc_set_lib_path(s, tccPath);
+        tcc_add_sysinclude_path(s, incPath);
+        tcc_add_library_path(s, libPath);
+        r = tcc_compile_string(s, sbSrc.items);
+        if (r==-1) goto end;
     }
-    tcc_delete(s);
+
+    if (rt==RT_DLL) {
+        r = tcc_output_file(s, outPath);
+        if (r==-1) goto end;
+    } else if (rt==RT_MEM) {
+        r = tcc_relocate(s);
+        if (r==-1) goto end;
+    }
+
+    if (rt==RT_MEM) {
+        ic_main = tcc_get_symbol(s, "ic_main");
+    } else {
+    #ifdef _WIN32
+        h = LoadLibraryA(outPath);
+        ic_main = (void *)GetProcAddress(h, "ic_main");
+    #else
+        h = dlopen(outPath, RTLD_NOW);
+        ic_main = (void *)dlsym(h, "ic_main");
+    #endif
+    }
+
+    if (ic_main!=NULL) {
+        r = ic_main(myArgsLen, myArgs);
+    }
+
+end:
+    if (h!=NULL) {
+    #ifdef _WIN32
+        FreeLibrary(h);
+    #else
+        dlclose(h);
+    #endif
+    }
+    if (s!=NULL) tcc_delete(s);
     nob_temp_rewind(mark);
 
     return r;
@@ -406,61 +548,9 @@ int Run(
 void AppendLineNum(StrBuilder *sb, usz line)
 {
     size_t mark = nob_temp_save();
-    char *buf = nob_temp_sprintf("#line %zu\n", line);
+    char *buf = nob_temp_sprintf("#line %zu \"<string>\"\n", line);
     nob_sb_append_cstr(sb, buf);
     nob_temp_rewind(mark);
-}
-
-// temp
-void ParseShell(char const *sh, usz len, Nob_Cmd *cmd)
-{
-    usz i;
-    int inQuote = 0, wasSpace = 0;
-    Nob_String_View sv = nob_sv_from_parts(sh, len);
-    StrBuilder sb = {0};
-
-    #define FINISH_ARG do {\
-        nob_sb_append_null(&sb);\
-        nob_da_append(cmd, nob_temp_strdup(sb.items));\
-        sb.count = 0;} while (0)
-
-    sv = nob_sv_trim(sv);
-    for (i = 0; i<sv.count; ++i) {
-        char c = sv.data[i];
-        if (inQuote==1) {
-            wasSpace = 0;
-            if (c!='\'') {
-                nob_da_append(&sb, c);
-            } else if (i+1<sv.count && sv.data[i+1]=='\'') {
-                nob_da_append(&sb, c); ++i;
-            } else {
-                inQuote = 0;
-            }
-        } else if (c=='\'') {
-            inQuote = 1;
-            wasSpace = 0;
-        } else if (c==' ') {
-            if (wasSpace==0) FINISH_ARG;
-            wasSpace = 1;
-        } else { 
-            wasSpace = 0;
-            nob_da_append(&sb, c);
-        }
-    }
-    FINISH_ARG;
-
-    #undef FINISG_ARG
-    nob_sb_free(sb);
-}
-
-void SpawnShell(char const *sh, usz len)
-{
-    Nob_Cmd cmd = {0};
-    size_t mark = nob_temp_save();
-    ParseShell(sh, len, &cmd);
-    nob_cmd_run_sync(cmd);
-    nob_temp_rewind(mark);
-    nob_da_free(cmd);
 }
 
 int main(int argc, char **argv)
@@ -473,17 +563,24 @@ int main(int argc, char **argv)
     char const *tccPath = GetExePath();
     char const *incPath = nob_temp_sprintf("%s/include", tccPath);
     char const *libPath = nob_temp_sprintf("%s/lib", tccPath);
-
-    nob_minimal_log_level = NOB_WARNING;
+    RunType rt = RT_MEM;
 
     for (int i = 1; i<argc; ++i) {
         char *a = argv[i];
         if (strcmp(a, "-h")==0 || strcmp(a, "--help")==0) {
             printf(
-                "Usage: %s [compiler options...] [-- program argv...]\n"
+                "Usage: %s [cc | dll] [compiler options...] [-- program argv...]\n"
                 , argv[0]
             );
             return 0;
+        }
+        if (strcmp(a, "dll")==0) {
+            rt = RT_DLL;
+            continue;
+        }
+        if (strcmp(a, "cc")==0) {
+            rt = RT_CC;
+            continue;
         }
         if (strcmp(a, "--")==0) argStart = 1;
         else if (argStart) nob_da_append(&arg, a);
@@ -598,14 +695,11 @@ int main(int argc, char **argv)
                 nob_sb_append_buf(&first, out.items, out.count);
             }
         run_label:
-            ok = Run(
+            ok = Run(rt,
                 tccPath, incPath, libPath, line,
-                opt.items, opt.count,
-                arg.items, arg.count,
-                pre.items, pre.count,
-                first.items, first.count,
-                src.items, src.count,
-                last.items, last.count) >= 0;
+                &opt, &arg,
+                &pre, &first,
+                &src, &last) >= 0;
             if (ok) {
                 if (kind==Stmt) {
                     line = outLine;
